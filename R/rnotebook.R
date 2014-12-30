aeac10cf794aeccd4a0604043f2ac88899fc010a
Yihui Xie <xie@yihui.name>
2014-12-30 14:43:30 -0600

    move newnb() to the beginning

--- R/rnotebook.R
+++ R/rnotebook.R
@@ -1,3 +1,56 @@
+#' Create a new notebook file
+#'
+#' Create a notebook file based on the metadata (e.g.,title and author, etc) and
+#' the content.
+#' @inheritParams refresh
+#' @param title the title of the notebook
+#' @param author the author of the notebook
+#' @param date the date for the notebook
+#' @param ... other metadata for the notebook
+#' @param body the body of the notebook; a default list of content is used if
+#'   not provided, otherwise it should be a list of sub-lists, with each
+#'   sub-list being a list of elements \code{type} (\code{text} or \code{code}),
+#'   \code{src} (source), and \code{out} (output); for \code{type = 'code'},
+#'   \code{src} is a list of two elements \code{options} (chunk options) and
+#'   \code{code} (source code)
+#' @return The notebook filename (a temporary file by default, with the prefix
+#'   \code{rnotebook}). The metadata and content are written to the file as a
+#'   JSON string.
+#' @export
+#' @examples # !formatR
+#' f = rnotebook::newnb()
+#' rnotebook::refresh(f)  # compile the notebook
+#' file.show(f)
+#'
+#' f2 = rnotebook::newnb('foo-test.Rnb', author = 'R. A. Fisher', body = list(
+#'   list(type = 'text', src = 'Hello **world**.'),
+#'   list(type = 'code', src = list(
+#'     options = '', code = 'fisher.test(matrix(c(3, 1, 1, 3), 2))'
+#'   ))
+#' ))
+#' file.show(f2)
+newnb = function(
+  file = tempfile('rnotebook', '.', '.Rnb'),
+  title = getOption('rnotebook.title', 'An R Notebook'),
+  author = getOption('rnotebook.author'),
+  date = Sys.Date(),
+  ...,
+  body
+) {
+  if (missing(body))
+    body = list(
+      list(type = 'text', src = 'A _sample_ paragraph.', out = ''),
+      list(type = 'code', src = list(options = 'tidy = TRUE', code = '1 + 1; x = dnorm(0)'), out = ''),
+      list(type = 'text', src = c('We know the density of N(0, 1)', 'at 0 is `r x`.'), out = '')
+    )
+  info = list(
+    frontmatter = list(title = title, author = author, date = as.character(date), ...),
+    body = body
+  )
+  write_json(info, file)
+  invisible(file)
+}
+
 #' Update a notebook file
 #'
 #' Parse and recompile a notebook file through \pkg{knitr}.
@@ -97,57 +150,4 @@ export = function(file, output, raw = FALSE) {
 
   writeLines(out, output, useBytes = TRUE)
   invisible(output)
-}
-
-#' Create a new notebook file
-#'
-#' Create a notebook file based on the metadata (e.g.,title and author, etc) and
-#' the content.
-#' @inheritParams refresh
-#' @param title the title of the notebook
-#' @param author the author of the notebook
-#' @param date the date for the notebook
-#' @param ... other metadata for the notebook
-#' @param body the body of the notebook; a default list of content is used if
-#'   not provided, otherwise it should be a list of sub-lists, with each
-#'   sub-list being a list of elements \code{type} (\code{text} or \code{code}),
-#'   \code{src} (source), and \code{out} (output); for \code{type = 'code'},
-#'   \code{src} is a list of two elements \code{options} (chunk options) and
-#'   \code{code} (source code)
-#' @return The notebook filename (a temporary file by default, with the prefix
-#'   \code{rnotebook}). The metadata and content are written to the file as a
-#'   JSON string.
-#' @export
-#' @examples # !formatR
-#' f = rnotebook::newnb()
-#' rnotebook::refresh(f)  # compile the notebook
-#' file.show(f)
-#'
-#' f2 = rnotebook::newnb('foo-test.Rnb', author = 'R. A. Fisher', body = list(
-#'   list(type = 'text', src = 'Hello **world**.'),
-#'   list(type = 'code', src = list(
-#'     options = '', code = 'fisher.test(matrix(c(3, 1, 1, 3), 2))'
-#'   ))
-#' ))
-#' file.show(f2)
-newnb = function(
-  file = tempfile('rnotebook', '.', '.Rnb'),
-  title = getOption('rnotebook.title', 'An R Notebook'),
-  author = getOption('rnotebook.author'),
-  date = Sys.Date(),
-  ...,
-  body
-) {
-  if (missing(body))
-    body = list(
-      list(type = 'text', src = 'A _sample_ paragraph.', out = ''),
-      list(type = 'code', src = list(options = 'tidy = TRUE', code = '1 + 1; x = dnorm(0)'), out = ''),
-      list(type = 'text', src = c('We know the density of N(0, 1)', 'at 0 is `r x`.'), out = '')
-    )
-  info = list(
-    frontmatter = list(title = title, author = author, date = as.character(date), ...),
-    body = body
-  )
-  write_json(info, file)
-  invisible(file)
 }
